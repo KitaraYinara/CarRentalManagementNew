b99daaa192f2766faed8d064782a1c0b556c0c4c
LEE YIN KIT 2204165A <2204165A@student.tp.edu.sg>
2023-11-22 12:10:49 +0800

    Added database and default data.

---
+++ CarRentalManagement/Shared/Domain/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace CarRentalManagement.Shared.Domain
+{
+    public class Vehicle : BaseDomainModel
+    {
+        public int year { get; set; }
+        public string? LicensePlateNumber { get; set; }
+        public int MakeId { get; set; }
+        public virtual Make? Make { get; set; }
+        public int ModelId { get; set;}
+        public virtual Model? Model { get; set; }
+        public int ColourId { get; set; }
+        public virtual Colour? Colour { get; set; }
+        public virtual List<Booking>? Bookings { get; set; }
+    }
+}
